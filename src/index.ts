07c27de4051d1cc0702b773fbe45e475ec057759
Teo Risso <68182473+teorisso@users.noreply.github.com>
2024-10-22 23:44:57 -0300

    update

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'; //ESModules
+
+import diaryRouter from './routes/diaries';
+
+const app = express();
+app.use(express.json()); //middleware que transforma la req.body a un json
+
+const PORT = 3000;
+
+
+
+app.get('/ping', (_req, res) => {
+    console.log('someone pinged here!!' + new Date().toLocaleDateString());
+    res.send('pong');
+});
+
+app.use('/api/diaries', diaryRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`);
+});
